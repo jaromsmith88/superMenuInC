70231ad7b45753832d9137a1abb9d7f890c43531
Jarom Smith <jaromsmith@mail.weber.edu>
2018-02-27 11:54:38 -0700

    Initial commit

---
+++ superMenu.c
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  superMenu.c
+ *
+ *    Description:  My Super Menu In C
+ *
+ *        Version:  1.0
+ *        Created:  02/27/2018 11:45:00 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jarom Smith (), jaromsmith@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    printf("Hello Super program\n");
+    return 0;
+}
+// Function Definitions
+
+
